5aa74c2dc35a42724ba3cdc474eb41a0aa9fad6d
lihy <huanyingtoyou@live.com>
2018-10-16 17:55:55 +0800

    1.增加网关 2.Hystrix Dashboard可视化监控

---
+++ view-zuul/src/main/java/com/lihy/view/zuul/application/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.lihy.view.zuul.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关
+ * @author lihy
+ * @date 2018/10/16
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
