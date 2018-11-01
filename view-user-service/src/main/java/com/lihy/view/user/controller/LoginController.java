0052df0761d1b3fe7e41ea61f32b11a0005aac98
lihy <huanyingtoyou@live.com>
2018-11-01 18:09:04 +0800

    一些写法上的修改

--- view-user-service/src/main/java/com/lihy/view/user/controller/LoginController.java
+++ view-user-service/src/main/java/com/lihy/view/user/controller/LoginController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     public static void main(String[] args) {
         int i = 1;
-        System.out.println(i++);
+        i = i++;
+        System.out.println(i);
+        //System.out.println(i++);
         System.out.println(++i);
     }
 }
